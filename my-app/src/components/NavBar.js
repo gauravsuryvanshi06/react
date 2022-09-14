cfd3bc8f8d8b4bd9c204ac268f0b46982a054cbb
G-5256 <gauravsuryvanshi06@gmail.com>
2022-09-15 00:00:07 +0530

    Bootstrapping

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -23,6 +23,19 @@ class NavBar extends React.Component {
             </div>
           </div>
         </nav>
+
+        <nav className="navbar navbar-light bg-light">
+  <form className="form-inline">
+    <button className="btn btn-outline-success" type="button">
+      Main button
+    </button>
+    <button className="btn btn-sm btn-outline-secondary" type="button">
+      Smaller button
+    </button>
+  </form>
+</nav>
+
+
       </div>
     );
   }
